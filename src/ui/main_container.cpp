905a7c78fbfbb874f7116c13dd17656ef0d0baec
AnthonyLandmesser <landmesseranthony@gmail.com>
2025-11-02 19:25:01 -0500

    basic button functionality

--- src/ui/main_container.cpp
+++ src/ui/main_container.cpp
@@ -1,14 +1,29 @@
 #include "ui/main_container.h"
-#include <QPushButton>
 #include <QVBoxLayout>
 
 MainContainer::MainContainer() {
-    QPushButton* button = new QPushButton(tr("Launch Gamepad Stream"));
+    launchText = tr("Launch Gamepad Stream");
+    closeText = tr("Close Gamepad Stream");
+    videoActive = false;
+
+    button = new QPushButton(launchText);
     QFont buttonFont = button->font();
     buttonFont.setPointSize(buttonFont.pointSize()*4);
     button->setFont(buttonFont);
+
+    connect(button, &QPushButton::clicked, this, &MainContainer::launchVideo);
     
     QVBoxLayout* layout = new QVBoxLayout;
     setLayout(layout);
     layout->addWidget(button, 0, Qt::AlignCenter);
+}
+
+void MainContainer::launchVideo() {
+    if (videoActive) {
+        button->setText(launchText);
+    }
+    else {
+        button->setText(closeText);
+    }
+    videoActive = !videoActive;
 }
--- src/ui/main_container.h
+++ src/ui/main_container.h
@@ -1,7 +1,16 @@
 #pragma once
 #include <QWidget>
+#include <QPushButton>
 
 class MainContainer : public QWidget {
+    Q_OBJECT
 public:
     MainContainer();
+private slots:
+    void launchVideo();
+private:
+    QString launchText;
+    QString closeText;
+    QPushButton* button;
+    bool videoActive;
 };
